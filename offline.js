1ac85a996c438eb7713db783c7434ba656daf186
Ur Mom GAY <sunweaver.ltd@gmail.com>
2024-10-21 07:47:09 +0300

    First Push

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1729485702,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/people-sheet0.png",
+		"images/mousecursor-sheet0.png",
+		"images/pop-sheet0.png",
+		"images/spawn_ini-sheet0.png",
+		"images/tomb-sheet0.png",
+		"media/pop.m4a",
+		"media/pop.ogg",
+		"media/porn.m4a",
+		"media/porn.ogg",
+		"media/crowd.m4a",
+		"media/crowd.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
